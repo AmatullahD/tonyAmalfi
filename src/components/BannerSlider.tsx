f214f0d3a486ee5043bb4c97f454900e2fe9675c
Amatullah <amatullahd99@gmail.com>
2026-04-20 15:14:26 +0530

    latest changes

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -10,39 +10,45 @@ const banners = [
 
 export default function BannerSlider() {
   const [current, setCurrent] = useState(0)
+  const [isHovered, setIsHovered] = useState(false)
 
   useEffect(() => {
+    if (isHovered) return
+
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % banners.length)
     }, 3000)
+
     return () => clearInterval(interval)
-  }, [])
+  }, [isHovered])
 
   return (
     <div className="w-full mt-10 px-4">
-      <div className="relative w-full h-[220px] sm:h-[300px] md:h-[380px] overflow-hidden">
+      <div
+        className="relative w-full aspect-[16/9] sm:aspect-[16/7] md:aspect-[16/6] overflow-hidden"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
 
         {banners.map((img, index) => (
           <img
             key={index}
             src={img}
             alt="banner"
-            className={`absolute w-full h-full object-cover transition-opacity duration-700 ${
-              current === index ? "opacity-100" : "opacity-0"
-            }`}
+            className={`absolute w-full h-full object-cover sm:object-cover transition-opacity duration-700 ${current === index ? "opacity-100" : "opacity-0"
+              }`}
           />
         ))}
 
-    
+
         {/* Dots */}
         <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-2">
           {banners.map((_, index) => (
             <div
               key={index}
               onClick={() => setCurrent(index)}
-              className={`h-2 w-2 rounded-full cursor-pointer ${
-                current === index ? "bg-white w-4" : "bg-white/50"
-              } transition-all`}
+              className={`h-2 w-2 rounded-full cursor-pointer ${current === index ? "bg-white w-4" : "bg-white/50"
+                } transition-all`}
             />
           ))}
         </div>
